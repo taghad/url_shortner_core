ba444d040df867b1ad9676cd5e1f46bb077c3890
mohamadHasan <mhtaghad99@gmail.com>
2020-02-08 10:32:04 +0330

    Add some functionalities for check using just alphabet in short_url and length of that most 8 chars and checking if not exist in database we are return notfound and at the end if every thing is ok we are redirect to long_url of this

--- src/Controllers/Redirector.cs
+++ src/Controllers/Redirector.cs
@@ -1,4 +1,39 @@
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using url_shortner.Models;
+using System.Text.RegularExpressions;
+
 namespace src.Controllers
 {
-
+    [Route("/redirector/{*short_url}")]
+    [ApiController]
+    public class RedirectController : Controller 
+    {
+         private readonly AppDbContext _context;
+        public RedirectController(AppDbContext context)
+        {
+        _context = context;
+        }
+        [HttpGet]
+        public ActionResult Get(string short_url) 
+        {    
+            Url url = new Url();
+            if(!Regex.IsMatch(short_url, @"^[a-zA-Z]+$")) 
+            {
+                return BadRequest();
+            }
+            if(short_url.Length != 8) 
+            {
+                return BadRequest();
+            }
+            if((_context.Find(url.GetType(),short_url) == null)) 
+            {  
+                return NotFound();
+            }
+            var u = (_context.urls.Find(short_url));
+            return Redirect(u.LongUrl);
+        }
+    }
 }
