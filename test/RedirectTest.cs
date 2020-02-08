08493a098cf2b4464bc53599a70fd912212f7cd8
mohamadHasan <mhtaghad99@gmail.com>
2020-02-08 09:44:02 +0330

    Add cheker for short url Length

--- test/RedirectTest.cs
+++ test/RedirectTest.cs
@@ -38,18 +38,56 @@ namespace url_shortner_core.Test
                 .TestStatus("Not Alphabetical", r => r == 400)
                 .AssertAll();
 
+            // new RestAssured()
+            //   .Given()
+            //     .Name("Is Alphabetical")
+            //     .Header("content-type", "application/json")
+            //   .When()
+            //     .Get("http://localhost:5000/redirect/EwmsBzbP")
+            //     .Then()
+            //     .TestStatus("Is Alphabetical", r => r == 302)
+            //     .AssertAll();
+        }
+        [Fact]
+        public void TestJust8Char()
+        {
             new RestAssured()
               .Given()
-                .Name("Is Alphabetical")
+                .Name("Less than 8 chars")
                 .Header("content-type", "application/json")
+                .Header("Accept-Encoding", "gzip, deflate, br")
               .When()
-                .Get("http://localhost:5000/redirect/EwmsBzbP")
+                
+                .Get("http://localhost:5000/redirect/ZkPRHb")
                 .Then()
-                .TestStatus("Is Alphabetical", r => r == 302)
+                .TestStatus("Less than 8 chars", r => r == 400)
+                .AssertAll();
+
+            new RestAssured()
+              .Given()
+                .Name("More than 8 chars")
+                .Header("content-type", "application/json")
+                .Header("Accept-Encoding", "gzip, deflate, br")
+              .When()
+                
+                .Get("http://localhost:5000/redirect/ZkPRHbDLghgfg")
+                .Then()
+                .TestStatus("More than 8 chars", r => r == 400)
+                .AssertAll();
+
+
+            new RestAssured()
+              .Given()
+                .Name("equal 8 chars")
+                .Header("content-type", "application/json")
+                .Header("Accept-Encoding", "gzip, deflate, br")
+              .When()
+                
+                .Get("http://localhost:5000/redirect/ZkPRHbDL")
+                .Then()
+                .TestStatus("Equal 8 chars", r => r == 200)
                 .AssertAll();
         }
-
-        
     }
 
 }
