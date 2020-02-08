612ed8997fa57feb628ffbb4bf2e0f65c8a04580
mohamadHasan <mhtaghad99@gmail.com>
2020-02-08 04:11:32 +0330

    Add alphabetical checker for short url

--- test/GetLongUrlTest.cs
+++ test/GetLongUrlTest.cs
@@ -1,8 +1,9 @@
 using System;
 using Xunit;
 using RA;
+using System.Text.RegularExpressions;
 
-namespace url_shortner_core
+namespace url_shortner_core.Test
 {
     public class GetLongUrlTest
     {
@@ -24,5 +25,24 @@ namespace url_shortner_core
                 .TestStatus("Status code Test", s => s == 200)
                 .AssertAll();                
         }
+
+        [Fact]
+        public void TestOnlyAlphabetsUsedInShort() {
+            var body = new {
+                LongUrl = "http://google.com"
+            };    
+            new RestAssured()
+             .Given()
+                .Name("Alphabets Checker")
+                .Header("Content-Type", "application/json")
+                .Body(body)
+            .When()
+                .Post("http://localhost:5000/get_long_url")
+            .Then()
+                .TestBody("Alphabet Test", u => Regex.IsMatch((string)u.shortUrl, @"^[a-zA-Z]+$"))
+                .TestStatus("Status code Test", s => s == 200)
+                .AssertAll();                
+        }
+        
     }
 }
